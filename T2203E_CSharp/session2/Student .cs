b487870ab700997fe550d43c37fc40361e4b7e71
Hoàng <HoangTHTH22020002@fpt.edu.vn>
2022-12-24 19:44:48 +0700

    update

---
+++ T2203E_CSharp/session2/Student .cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2203E_CSharp.session1
+{
+    internal class Student : Human
+    {
+        private string[] telephone = new string[3];
+
+        public Student()
+        {
+        }
+
+        public Student(string s, int a) : base(s)
+        {
+            this.Age = a;
+        }
+
+        public string this[int index] // indexer
+        {
+            get => telephone[index];
+            set => telephone[index] = value;
+        }
+
+        public void Study()
+        {
+            base.Run();
+            this.Run();
+        }
+
+        public override void Eat()
+        {
+
+        }
+
+    }
+}
